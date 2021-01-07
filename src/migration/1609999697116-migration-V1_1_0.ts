5dec3cb1a9fa4fdab5156bd7cc0210341af33dfe
shahar s <cl_shaharshavit@rnd-hub.com>
2021-01-07 08:09:44 +0200

    chore(migration): added first migration

---
+++ src/migration/1609999697116-migration-V1_1_0.ts
@@ -0,0 +1,24 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class migrationV1101609999697116 implements MigrationInterface {
+    name = 'migrationV1101609999697116'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "statuses" ("id" SERIAL NOT NULL, "taskId" character varying NOT NULL, "userId" character varying NOT NULL, "fileName" character varying NOT NULL, "directoryName" character varying NOT NULL, "fileURI" character varying NOT NULL, "progress" integer NOT NULL, "status" "statuses_status_enum" NOT NULL, "geometry" geometry(Geometry,4326) NOT NULL, "estimatedFileSize" double precision NOT NULL, "realFileSize" double precision NOT NULL, "creationTime" TIMESTAMP WITH TIME ZONE NOT NULL, "updatedTime" TIMESTAMP WITH TIME ZONE NOT NULL, "expirationTime" TIMESTAMP WITH TIME ZONE NOT NULL, "workerAttempts" integer NOT NULL DEFAULT 0, "maxZoom" integer NOT NULL DEFAULT 18, CONSTRAINT "PK_2fd3770acdb67736f1a3e3d5399" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "taskIndex" ON "statuses" ("taskId") `);
+        await queryRunner.query(`CREATE INDEX "userIndex" ON "statuses" ("userId") `);
+        await queryRunner.query(`CREATE INDEX "geometryIndex" ON "statuses" USING GiST ("geometry") `);
+        await queryRunner.query(`CREATE INDEX "expirationTimeIndex" ON "statuses" ("expirationTime") `);
+        await queryRunner.query(`CREATE UNIQUE INDEX "pathIndex" ON "statuses" ("fileName", "directoryName") `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "pathIndex"`);
+        await queryRunner.query(`DROP INDEX "expirationTimeIndex"`);
+        await queryRunner.query(`DROP INDEX "geometryIndex"`);
+        await queryRunner.query(`DROP INDEX "userIndex"`);
+        await queryRunner.query(`DROP INDEX "taskIndex"`);
+        await queryRunner.query(`DROP TABLE "statuses"`);
+    }
+
+}
