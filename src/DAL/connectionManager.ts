36872e26ae787d66d3411f1efe353ac9909cab56
Anguiar <shacharroy14@gmail.com>
2021-01-26 15:14:45 +0200

    chore: add ssl configuration to connection manager

--- src/DAL/connectionManager.ts
+++ src/DAL/connectionManager.ts
@@ -4,6 +4,8 @@ import { delay, inject, injectable } from 'tsyringe';
 import { MCLogger } from '@map-colonies/mc-logger';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { StatusesRepository } from './statusesRepository';
+import { promises as FsPromises } from 'fs';
+import { TlsOptions } from 'tls';
 
 @injectable()
 export class ConnectionManager {
@@ -13,6 +15,26 @@ export class ConnectionManager {
     @inject(delay(() => MCLogger)) private readonly logger: MCLogger
   ) {}
 
+  private async initSSLConnection(connectionConfig : PostgresConnectionOptions): Promise<void> {
+    const tlsConfigurations = connectionConfig.ssl as TlsOptions;
+    if (
+      tlsConfigurations !== undefined &&
+      tlsConfigurations.ca &&
+      tlsConfigurations.cert &&
+      tlsConfigurations.key
+    ) {
+      const encoding = 'utf-8';
+      const sslFiles = await Promise.all([
+        FsPromises.readFile(tlsConfigurations.ca as string, encoding),
+        FsPromises.readFile(tlsConfigurations.cert as string, encoding),
+        FsPromises.readFile(tlsConfigurations.key as string, encoding),
+      ]);
+      tlsConfigurations.ca =  sslFiles[0],
+      tlsConfigurations.cert = sslFiles[1],
+      tlsConfigurations.key = sslFiles[2] 
+    }
+  }
+
   public async init(): Promise<void> {
     const connectionConfig = config.get<PostgresConnectionOptions>('typeOrm');
     this.logger.info(
@@ -21,6 +43,7 @@ export class ConnectionManager {
       }`
     );
     try {
+      await this.initSSLConnection(connectionConfig);
       this.connection = await createConnection(connectionConfig);
     } catch (err) {
       const errString = JSON.stringify(err);
