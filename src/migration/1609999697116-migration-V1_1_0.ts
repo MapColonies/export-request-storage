8b2fb7536bc5bb46937b2376246f426322ac1e38
Shahar s <65118046+cl-shaharshavit-rnd@users.noreply.github.com>
2021-01-31 13:08:36 +0200

    fix(migration): fixed migration script  (#35)
    * fix(migration): fixed migration
    
    * fix(migration): fixed typeorm migration config
    
    * chore: code review

--- src/migration/1609999697116-migration-V1_1_0.ts
+++ src/migration/1609999697116-migration-V1_1_0.ts
@@ -4,6 +4,8 @@ export class migrationV1101609999697116 implements MigrationInterface {
     name = 'migrationV1101609999697116'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE EXTENSION  IF NOT EXISTS postgis;`);
+        await queryRunner.query(`CREATE TYPE "statuses_status_enum" AS ENUM('Pending', 'In-Progress', 'Completed', 'Failed')`);
         await queryRunner.query(`CREATE TABLE "statuses" ("id" SERIAL NOT NULL, "taskId" character varying NOT NULL, "userId" character varying NOT NULL, "fileName" character varying NOT NULL, "directoryName" character varying NOT NULL, "fileURI" character varying NOT NULL, "progress" integer NOT NULL, "status" "statuses_status_enum" NOT NULL, "geometry" geometry(Geometry,4326) NOT NULL, "estimatedFileSize" double precision NOT NULL, "realFileSize" double precision NOT NULL, "creationTime" TIMESTAMP WITH TIME ZONE NOT NULL, "updatedTime" TIMESTAMP WITH TIME ZONE NOT NULL, "expirationTime" TIMESTAMP WITH TIME ZONE NOT NULL, "workerAttempts" integer NOT NULL DEFAULT 0, "maxZoom" integer NOT NULL DEFAULT 18, CONSTRAINT "PK_2fd3770acdb67736f1a3e3d5399" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE UNIQUE INDEX "taskIndex" ON "statuses" ("taskId") `);
         await queryRunner.query(`CREATE INDEX "userIndex" ON "statuses" ("userId") `);
@@ -19,6 +21,7 @@ export class migrationV1101609999697116 implements MigrationInterface {
         await queryRunner.query(`DROP INDEX "userIndex"`);
         await queryRunner.query(`DROP INDEX "taskIndex"`);
         await queryRunner.query(`DROP TABLE "statuses"`);
+        await queryRunner.query(`DROP TYPE "statuses_status_enum"`);
     }
 
 }
--- src/migration/generateMigrationConfig.js
+++ src/migration/generateMigrationConfig.js
@@ -1,11 +1,5 @@
 const config = require('config');
 const fs = require('fs');
-const exec = require('child_process').exec;
-let version = require('../../package.json').version;
-
-const replacePattern = new RegExp('[.]','g');
-version = version.replace(replacePattern,'_');
-const name = `migration-V${version}`;
 
 const ormConfig = config.get('typeOrm');
 if (ormConfig.ssl && ormConfig.ssl.ca && ormConfig.ssl.cert && ormConfig.ssl.key) {
@@ -22,14 +16,3 @@ if(fs.existsSync('./src/migration/ormConfig.json')){
     fs.unlinkSync('./src/migration/ormConfig.json');
 }
 fs.writeFileSync('./src/migration/ormConfig.json',JSON.stringify(ormConfig));
-exec(`npm run typeorm -- migration:generate -f ./src/migration/ormConfig.json -n ${name}`,
-(err, stdout, stderr) => {
-    if (err) {
-      //some err occurred
-      console.error(err)
-    } else {
-     // the *entire* stdout and stderr (buffered)
-     console.log(`stdout: ${stdout}`);
-     console.log(`stderr: ${stderr}`);
-    }
-})
